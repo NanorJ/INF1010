57b816254478419c410af6d1022d6c58452497f7
NanorJ <39227727+NanorJ@users.noreply.github.com>
2018-11-15 10:42:36 -0500

    modifications de gestionnaire Utilisateurrrrr

--- TD5/gestionnaireUtilisateurs.cpp
+++ TD5/gestionnaireUtilisateurs.cpp
@@ -8,11 +8,11 @@
 #include "utilisateurPremium.h"
 #include "utilisateurRegulier.h"
 
-vector<double> getComptes() const{}
-int estExistant(Utilisateur* utilisateur) const{}
-void mettreAJourComptes(Utilisateur* payePar, double montant){}
-pair<Utilisateur*, double>& getMax() const{}
-pair<Utilisateur*, double>& getMin() const{}
-Utilisateur * getUtilisateurSuivant(Utilisateur* utilisateur, double montant) const{} 
-vector<pair<Utilisateur*, double>> getUtilisateursEntre(double borneInf, double borneSup) const{} 
-GestionnaireUtilisateurs & setCompte(pair<Utilisateur*, double> p){}
+vector<double> gestionnaireUtilisateurs:: getComptes() const{}
+int gestionnaireUtilisateurs::estExistant(Utilisateur* utilisateur) const{}
+voidgestionnaireUtilisateurs::mettreAJourComptes(Utilisateur* payePar, double montant){}
+pair<Utilisateur*, double>& gestionnaireUtilisateurs::getMax() const{}
+pair<Utilisateur*, double>& gestionnaireUtilisateurs::getMin() const{}
+Utilisateur * gestionnaireUtilisateurs::getUtilisateurSuivant(Utilisateur* utilisateur, double montant) const{}
+vector<pair<Utilisateur*, double>> gestionnaireUtilisateurs::getUtilisateursEntre(double borneInf, double borneSup) const{}
+GestionnaireUtilisateurs & gestionnaireUtilisateurs::setCompte(pair<Utilisateur*, double> p){}
