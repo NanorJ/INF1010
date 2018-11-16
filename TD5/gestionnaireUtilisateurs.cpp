138bd057152c3c0e07940e769426fbaa097819f4
Nanor Janjikian <39227727+NanorJ@users.noreply.github.com>
2018-11-16 10:15:43 -0500

    Update gestionnaireUtilisateurs.cpp

--- TD5/gestionnaireUtilisateurs.cpp
+++ TD5/gestionnaireUtilisateurs.cpp
@@ -2,7 +2,7 @@
 * Titre: Travail pratique #5 - gestionnaireUtilisateurs.cpp
 * Date: 4 novembre 2018
 * Auteur: Ryan Hardie
-*******************************************/
+********************************************/
 
 #include "gestionnaireUtilisateurs.h"
 #include "utilisateurPremium.h"
